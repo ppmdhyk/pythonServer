2401e1e03fe0c812ef6759d0f24515c12df30d63
dhyk <dikarizky66@gmail.com>
2020-01-28 19:54:59 +0700

    dika

---
+++ klasifikasi.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+
+filepath_dict = {'yelp':   'data/yelp.txt',
+                 'amazon': 'data/amazon.txt',
+                 'imdb':   'data/imdb.txt'}
+
+df_list = []
+for source, filepath in filepath_dict.items():
+    df = pd.read_csv(filepath, names=['sentence', 'label'], sep='\t')
+    df['source'] = source  # Add another column filled with the source name
+    df_list.append(df)
+
+df = pd.concat(df_list)
+
+for source in df['source'].unique():
+    df_source = df[df['source'] == source]
+    sentences = df_source['sentence'].values
+    y = df_source['label'].values
+
+    sentences_train, sentences_test, y_train, y_test = train_test_split(sentences, y, test_size=0.25, random_state=1000)
+
+    vectorizer = CountVectorizer()
+    vectorizer.fit(sentences_train)
+    X_train = vectorizer.transform(sentences_train)
+    X_test  = vectorizer.transform(sentences_test)
+
+#------------------------------------------------------------
+
+classifier = LogisticRegression()
+classifier.fit(X_train, y_train)
+print("\n \n hasil \n -----------------------\n")
+print(classifier.predict(vectorizer.transform(['hai im good'])))
